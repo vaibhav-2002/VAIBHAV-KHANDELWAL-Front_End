38564726a4611f52674a94ffe1e76d05fec92ef2
vaibhav-2002 <vaibhavkhandelwal2002@gmail.com>
2023-04-23 21:59:46 +0530

    deploy fix

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import List from "./List"
-import imgg from "../SteelEye_Logo.jpg";
+import React from "react";
+import List from "./List";
+// import img from "../SteelEye_Logo.jpg";
 const Home = () => {
   return (
-    <div  className="App"  style={{ display:"flex" ,flexDirection:"column" , alignItems:"center"}}>
-     
-      
+    <div
+      className="App"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <h1>Output of the given Assignment</h1>
-     <List  />
-    
-     <p   style={{color:"solid-black"}}>hint: click on the any above background colour will be changed into Green. </p>
+      <List />
 
+      <p style={{ color: "solid-black" }}>
+        hint: click on the any above background color will be changed into
+        Green.{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
